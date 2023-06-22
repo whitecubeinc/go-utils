e9573cae8f420d99c77d2e3b474c2c8d052a1e16
chanbroK <koc0819@gmail.com>
2023-06-22 16:29:12 +0900

    feat: add NewStringEnum and NewStringEnumFromTag

--- reflect.go
+++ reflect.go
@@ -1,6 +1,8 @@
 package utils
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func GetNotEmptyFields(v any) (fields []string) {
 	reflectType := reflect.TypeOf(v)
@@ -19,4 +21,114 @@ func GetNotEmptyFields(v any) (fields []string) {
 	}
 
 	return
+}
+
+// NewStringEnum strings.ToUpper, strings.ToLower 및 strcase.ToXXX 사용
+func NewStringEnum[T any](keyCaseFunction func(string) string, valueCaseFunction func(string) string) T {
+	// FIXME add test code
+	dest := new(T)
+
+	destTypeOf := Deref(reflect.TypeOf(dest))
+	value := reflect.ValueOf(dest)
+
+	direct := reflect.Indirect(value)
+	base := Deref(value.Type())
+
+	vp := reflect.New(base)
+	v := reflect.Indirect(vp)
+
+	valuesIdx := -1
+	values := make(map[string]string)
+
+	for i := 0; i < destTypeOf.NumField(); i++ {
+		switch v.Field(i).Type().Kind() {
+		case reflect.String:
+			valuePtr := reflect.New(v.Field(i).Type())
+			fieldValue := reflect.Indirect(valuePtr)
+
+			value := destTypeOf.Field(i).Name
+			if valueCaseFunction != nil {
+				value = valueCaseFunction(destTypeOf.Field(i).Name)
+			}
+
+			key := destTypeOf.Field(i).Name
+			if keyCaseFunction != nil {
+				key = keyCaseFunction(destTypeOf.Field(i).Name)
+			}
+			values[key] = value
+
+			fieldValue.SetString(value)
+			v.Field(i).Set(fieldValue)
+		case reflect.Map:
+			if destTypeOf.Field(i).Name == "ValueMap" {
+				valuesIdx = i
+			}
+		}
+	}
+
+	if valuesIdx >= 0 {
+		v.Field(valuesIdx).Set(reflect.ValueOf(values))
+	}
+
+	direct.Set(v)
+
+	return *dest
+}
+
+// NewConstantFromTag tag key = value , string only
+func NewConstantFromTag[T any](keyCaseFunction func(string) string) T {
+	// FIXME add test code
+	dest := new(T)
+
+	destTypeOf := Deref(reflect.TypeOf(dest))
+
+	value := reflect.ValueOf(dest)
+
+	direct := reflect.Indirect(value)
+	base := Deref(value.Type())
+
+	vp := reflect.New(base)
+	v := reflect.Indirect(vp)
+
+	valuesIdx := -1
+	values := make(map[string]string)
+
+	for i := 0; i < destTypeOf.NumField(); i++ {
+		switch v.Field(i).Type().Kind() {
+		case reflect.String:
+			valuePtr := reflect.New(v.Field(i).Type())
+			fieldValue := reflect.Indirect(valuePtr)
+
+			value := destTypeOf.Field(i).Tag.Get("value")
+			key := destTypeOf.Field(i).Name
+			if keyCaseFunction != nil {
+				key = keyCaseFunction(destTypeOf.Field(i).Name)
+			}
+			values[key] = value
+
+			fieldValue.SetString(value)
+			v.Field(i).Set(fieldValue)
+		case reflect.Map:
+			if destTypeOf.Field(i).Name == "ValueMap" {
+				valuesIdx = i
+			}
+		}
+	}
+
+	if valuesIdx >= 0 {
+		v.Field(valuesIdx).Set(reflect.ValueOf(values))
+	}
+
+	direct.Set(v)
+
+	return *dest
+}
+
+func Deref(t reflect.Type) reflect.Type {
+	// FIXME add test code
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	return t
 }
