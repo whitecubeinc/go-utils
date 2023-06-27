12fb2a47f818bc3a49a7cee646b559d8764df880
chanbroK <koc0819@gmail.com>
2023-06-27 22:22:41 +0900

    feat: add struct2MWithCase utils

--- map.go
+++ map.go
@@ -6,3 +6,14 @@ func Struct2M(v any) map[string]any {
 	b := MarshalMust(v)
 	return ReturnUnmarshal[M](b)
 }
+
+// Struct2MWithCase string case function 으로 key 값을 변경하여 반환
+func Struct2MWithCase(v any, keyCaseFunction func(string) string) map[string]any {
+	mapData := Struct2M(v)
+	for key, value := range mapData {
+		mapData[keyCaseFunction(key)] = value
+		delete(mapData, key)
+	}
+
+	return mapData
+}
