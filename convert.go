138ef5e1023cb1c9ae17f9ad80dabe67c5c387ce
chanbroK <koc0819@gmail.com>
2023-06-30 00:40:17 +0900

    feat: add Any2String function

---
+++ convert.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"strconv"
+)
+
+func AnyToString(v any) (string, error) {
+	switch s := v.(type) {
+	case string:
+		return s, nil
+	case bool:
+		return strconv.FormatBool(s), nil
+	case int:
+		return strconv.Itoa(s), nil
+	case int64:
+		return strconv.FormatInt(s, 10), nil
+	case int32:
+		return strconv.Itoa(int(s)), nil
+	case int16:
+		return strconv.FormatInt(int64(s), 10), nil
+	case int8:
+		return strconv.FormatInt(int64(s), 10), nil
+	case uint:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint64:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint32:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint16:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case uint8:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case float64:
+		return strconv.FormatFloat(s, 'f', -1, 64), nil
+	case float32:
+		return strconv.FormatFloat(float64(s), 'f', -1, 32), nil
+	case json.Number:
+		return s.String(), nil
+	case []byte:
+		return string(s), nil
+	case template.HTML:
+		return string(s), nil
+	case template.HTMLAttr:
+		return string(s), nil
+	case template.URL:
+		return string(s), nil
+	case template.JS:
+		return string(s), nil
+	case template.JSStr:
+		return string(s), nil
+	case template.CSS:
+		return string(s), nil
+	case template.Srcset:
+		return string(s), nil
+	case nil:
+		return "", nil
+	case fmt.Stringer:
+		return s.String(), nil
+	case error:
+		return s.Error(), nil
+	default:
+		return "", errors.New(fmt.Sprintf("unable to cast %#v of type %T to string", v, v))
+	}
+}
