5de8abed8d02963818532363ec914c85b8adf0eb
chanbroK <koc0819@gmail.com>
2023-10-23 09:54:08 +0900

    feat: add get utils for http

--- request.go
+++ request.go
@@ -46,6 +46,47 @@ func Post[T any](url string, body any, header http.Header) (resBody T) {
 	return
 }
 
+func Get[T any](url string, param map[string]string, header http.Header) (resBody T) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	if param != nil {
+		query := req.URL.Query()
+		for k, v := range param {
+			query.Add(k, v)
+		}
+		req.URL.RawQuery = query.Encode()
+	}
+
+	// set header
+	req.Header = header
+
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	// read body
+	resBodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	resBody = ReturnUnmarshal[T](resBodyBytes)
+
+	return
+}
+
 func PostWithoutResponse(url string, body any, header http.Header) {
 	client := new(http.Client)
 
