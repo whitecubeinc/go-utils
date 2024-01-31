1a98991386771881f5bb6b4db1d654d74f9cb9a3
chanbroK <koc0819@gmail.com>
2024-01-31 11:07:14 +0900

    feat: add set utils function

--- set.go
+++ set.go
@@ -1,18 +1,59 @@
 package utils
 
-// GetDifferenceSet 차집합 arr1 - arr2 (comparable type만 가능합니다..), arr1 순서 보장
-func GetDifferenceSet[T comparable](arr1, arr2 []T) []T {
-	differSet := make([]T, 0, len(arr1))
-	existMap := make(map[T]bool, 0)
+// GetDifferenceSet 차집합, set1 - set1
+func GetDifferenceSet[V comparable](set1, set2 []V) (result []V) {
+	result = make([]V, 0, len(set1))
+	existMap := make(map[V]bool)
 
-	for _, v := range arr2 {
+	for _, v := range set2 {
 		existMap[v] = true
 	}
 
-	for _, v := range arr1 {
+	for _, v := range set1 {
 		if !existMap[v] {
-			differSet = append(differSet, v)
+			result = append(result, v)
 		}
 	}
-	return differSet
+	return result
+}
+
+// GetIntersectionSet 교집합
+func GetIntersectionSet[V comparable](set1, set2 []V) (result []V) {
+	result = make([]V, 0, len(set1))
+	table := make(map[V]bool)
+
+	for _, ele := range set1 {
+		table[ele] = true
+	}
+
+	for _, ele := range set2 {
+		if table[ele] {
+			result = append(result, ele)
+		}
+	}
+
+	return result
+}
+
+func GetUnionSet[V comparable](set1, set2 []V) (result []V) {
+	existMap := make(map[V]bool)
+	result = make([]V, 0, len(set1))
+	for _, v := range set1 {
+		if _, exist := existMap[v]; exist {
+			continue
+		}
+
+		result = append(result, v)
+		existMap[v] = true
+	}
+
+	for _, v := range set2 {
+		if _, exist := existMap[v]; exist {
+			continue
+		}
+
+		result = append(result, v)
+		existMap[v] = true
+	}
+	return
 }
