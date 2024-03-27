198f3cd365b303e69fe81f301d858187a3682fd8
chanbroK <koc0819@gmail.com>
2024-03-27 17:15:39 +0900

    feat: request Put 유틸 함수 추가

--- request.go
+++ request.go
@@ -106,3 +106,43 @@ func PostWithoutResponse(url string, body any, header http.Header) {
 		panic(err)
 	}
 }
+
+func Put[T any](url string, body any, header http.Header) (resBody T) {
+	if body == nil {
+		body = map[string]string{}
+	}
+
+	client := new(http.Client)
+
+	bodyBytes := MarshalMust(body)
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	// set header
+	req.Header = header
+
+	// send request
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	// read body
+	resBodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	resBody = ReturnUnmarshal[T](resBodyBytes)
+
+	return
+}
